e8cd2d37d1c16f490e83ff52b736d528e84f3d9c
Pyoneru <piotrkniemczuk@gmail.com>
2021-02-07 16:55:58 +0100

    Initial Repository
    Create empty project with .Net Core

---
+++ TheMerchant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheMerchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
