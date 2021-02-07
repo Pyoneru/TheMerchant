e298a8a7fab9a4cd38c196749effa9662db7e0ae
Pyoneru <piotrkniemczuk@gmail.com>
2021-02-07 18:12:11 +0100

    Create Product Model
    Create Product and Category class.
    Every class contains ID field.
    Id should be name of product/category in english and
    all characters will be in lower case.
    Product contains single Category reference.
    Override equals methods. Equals methods work only with ID field.

---
+++ TheMerchant/Model/Category.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Model
+{
+    /// <summary>
+    /// Class represent category of product.
+    /// Group many products in one group. e.g. For specific merchant wchich sell or buy only food.
+    /// </summary>
+    public class Category : IEquatable<Category>
+    {
+        /// <summary>
+        /// Id is name of category in english and his all charcaters are lower case.
+        /// Use in search in list.
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Name of category
+        /// </summary>
+        public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Category);
+        }
+        public bool Equals(Category other)
+        {
+            return other != null &&
+                   ID == other.ID;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID);
+        }
+    }
+}
---
+++ TheMerchant/Model/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Model
+{
+    /// <summary>
+    /// Class represent all products which we can buy or sell in game.
+    /// </summary>
+    public class Product : IEquatable<Product>
+    {
+        /// <summary>
+        /// Id is name of product in english and all characters are lower case.
+        /// Use in search product in product list.
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Name is displayed for user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description for product
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Reference to category
+        /// </summary>
+        public Category category { get; set;}
+
+        /// <summary>
+        /// Original price(not changed by merchants, events, etc.)
+        /// </summary>
+        public Decimal originalPrice { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Product);
+        }
+        public bool Equals(Product other)
+        {
+            return other != null &&
+                   ID == other.ID;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID);
+        }
+    }
+}
