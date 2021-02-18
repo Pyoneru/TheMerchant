a138a24f2e6329fc74a8560732743dde19f2e9ff
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 17:20:55 +0100

    Add MerchantNode class

---
+++ TheMerchant/Node/MerchantNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheMerchant.Model;
+
+namespace TheMerchant.Node
+{
+    public class MerchantNode : Node, IGold, IEquipment
+    {
+        public string Name { get; set; }
+        public Dictionary<MerchantProduct,int> DefaultProducts { get; set; }
+        public Decimal DefaultGold { get; set; }
+
+        public MerchantNode(string name, Node parent = null, params Node[] children) : base(name, parent, children)
+        {
+        }
+
+        public decimal Gold { get; set; }
+        public Dictionary<Product, int> Products { get; set; }
+        public decimal Weight { get; set; }
+        public decimal MaxWeight { get; set; }
+    }
+}
