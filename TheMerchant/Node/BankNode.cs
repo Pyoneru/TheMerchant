b71c0f26884d474b0420abc35966f3efae6a540f
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 16:50:26 +0100

    Add BankNode class

---
+++ TheMerchant/Node/BankNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Node
+{
+    public class BankNode : Node
+    {
+        public BankNode(string name, Node parent = null, params Node[] children) : base(name, parent, children)
+        {
+            
+        }
+
+        public string Name { get; set; }
+        public Decimal Debt { get; set; }
+    }
+}
