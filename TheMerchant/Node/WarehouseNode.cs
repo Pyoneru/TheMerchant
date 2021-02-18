80f6f39665114b40969f26a6ff27e9bd2c3fde52
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 17:41:57 +0100

    Add WarehouseNode class

---
+++ TheMerchant/Node/WarehouseNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheMerchant.Model;
+
+namespace TheMerchant.Node
+{
+    public class WarehouseNode : Node, IGold, IEquipment
+    {
+        public WarehouseNode(string name, Node parent = null, params Node[] children) : base(name, parent, children)
+        {
+        }
+
+        public decimal Gold { get; set; }
+        public Dictionary<Product, int> Products { get; set; }
+        public decimal Weight { get; set; }
+        public decimal MaxWeight { get; set; }
+    }
+}
