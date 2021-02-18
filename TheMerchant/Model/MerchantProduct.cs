9f326a93976484eda616fe5c1f28e71bdcb74d35
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 16:38:28 +0100

    Add MerchantProduct class

---
+++ TheMerchant/Model/MerchantProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Model
+{
+    public class MerchantProduct
+    {
+        public Product Product { get; set; }
+        public Decimal BuyRate { get; set; }
+        public Decimal SellRate { get; set; }
+    }
+}
