ff4def2fee22022a29ffe9a959b9a9ffe299db35
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 17:30:47 +0100

    Add PlayerNode class

---
+++ TheMerchant/Node/PlayerNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheMerchant.Model;
+
+namespace TheMerchant.Node
+{
+    public class PlayerNode : Node, IGold, IEquipment
+    {
+        public string Name { get; set; }
+        public string TownID { get; set; }
+
+        public PlayerNode(string name, Node parent = null, params Node[] children) : base(name, parent, children)
+        {
+
+        }
+
+        public decimal Gold { get; set; }
+        public Dictionary<Product, int> Products { get; set; }
+        public decimal Weight { get; set; }
+        public decimal MaxWeight { get; set; }
+    }
+}
