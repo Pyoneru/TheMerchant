eb3edb6a2e284213d54f2bc03f5e72156f4792a1
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 16:57:17 +0100

    Add HomeNode class

---
+++ TheMerchant/Node/HomeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Node
+{
+    public class HomeNode : Node
+    {
+        public HomeNode(string name, Node parent = null, params Node[] children) : base(name, parent, children)
+        {
+        }
+
+        public string Name { get; set; }
+        public bool Owned { get; set; }
+    }
+}
