c5a4aa06f7e7f087b42e036a6ee791358bc31224
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 16:31:08 +0100

    Add IEquipment interface
    Include map of products within Products property.
    Add Weight and MaxWeight Decimal members.

---
+++ TheMerchant/Model/IEquipment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Model
+{
+    public interface IEquipment
+    {
+        public Dictionary<Product,int> Products { get; set; }
+        public Decimal Weight { get; set; }
+        public Decimal MaxWeight { get; set; }
+    }
+}
