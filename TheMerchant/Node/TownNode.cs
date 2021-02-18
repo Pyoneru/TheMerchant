649d9fb8a9c54091a50b18be3ed9b7bba3337cbb
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 16:54:49 +0100

    Add TownNode class

---
+++ TheMerchant/Node/TownNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Node
+{
+    class TownNode : Node
+    {
+        public TownNode(string name, Node parent = null, params Node[] children) : base(name, parent, children)
+        {
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
