f4ed766e83428851f219a91aa709de9bbad7e801
LazzR <41189421+appollock@users.noreply.github.com>
2021-02-18 16:23:41 +0100

    Add IGold interface
    Add Gold property inside IGold interface

---
+++ TheMerchant/Model/IGold.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMerchant.Model
+{
+    public interface IGold
+    {
+        public Decimal Gold { get; set; }
+    }
+}
